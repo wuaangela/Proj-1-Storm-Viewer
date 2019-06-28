a8d5925cb9942e2ea3cb5399fad782fd32c880a8
A W <waq.mmhy@live.ca>
2019-06-27 23:00:14 -0400

    Add sharing function and update info.plist to download to Photos album

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -24,6 +24,8 @@ class DetailViewController: UIViewController {
         }
         
         title = "Picture \(selectedPictureNumber) of \(totalPictures)"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         navigationItem.largeTitleDisplayMode = .never
     }
     
@@ -35,6 +37,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 
